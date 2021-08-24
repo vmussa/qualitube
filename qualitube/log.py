a3d8e28d7191400d6da35ee802c18a2cd6eceb9b
Vitor Mussa <vtrmussa@gmail.com>
2021-08-23 21:03:53 -0300

    Isolate log handlers setup

--- qualitube/log.py
+++ qualitube/log.py
@@ -1,7 +1,3 @@
 import logging
-import sys
 
 logger = logging.getLogger('qualitube')
-logger.setLevel('INFO')
-logger.addHandler(logging.FileHandler('pipeline.log'))
-logger.addHandler(logging.StreamHandler(sys.stdout))
--- qualitube/main.py
+++ qualitube/main.py
@@ -3,30 +3,42 @@ from configparser import ConfigParser
 from .videos import Videos
 import pandas as pd
 from .log import logger
+import logging
 import sys
+
 
 config = ConfigParser()
 config.read("config.ini")
-CHANNEL_IDS = config['channels']['ids'].split("\n")[1:]
+CHANNEL_IDS = config["channels"]["ids"].split("\n")[1:]
+
+
+def set_logger(logger):
+    logger.setLevel("INFO")
+    logger.addHandler(logging.FileHandler("pipeline.log"))
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+
 
 def get_playlist_items_objs(channel_ids: str) -> PlaylistItems:
     """Get list of PlaylistItems objects from channel ids."""
     objs = [PlaylistItems(channel_id) for channel_id in channel_ids]
     return objs
 
+
 def chunks(lst: list, n) -> list:
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
+
 def main():
     """Main function."""
-    logger.info('Beginning of pipeline.')
+    set_logger(logger)
+    logger.info("Beginning of pipeline.")
 
     playlists = get_playlist_items_objs(channel_ids=CHANNEL_IDS)
     dfs = [playlist.to_df() for playlist in playlists]
     raw = pd.concat(dfs)
-    videos_ids_lst = raw['id'].to_list()
+    videos_ids_lst = raw["id"].to_list()
     chunked = list(chunks(videos_ids_lst, 50))
 
     dfs = []
@@ -34,9 +46,10 @@ def main():
         dfs.append(Videos(chunk).to_df())
 
     df = pd.concat(dfs).reset_index(drop=True)
-    df.to_csv('corpus.csv', index=False)
+    df.to_csv("corpus.csv", index=False)
 
-    logger.info('End of pipeline.')
+    logger.info("End of pipeline.")
+
 
 if __name__ == "__main__":
     main()
